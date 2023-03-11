a899fb44d0e9f5630873dd04604765c9de9d03f5
joe-trung <kieutrungtran@gmail.com>
2023-03-11 12:45:10 -0500

    Done Dice. Still have Bin and Simulation

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,24 @@
-public class Dice {
+import java.util.Random;
 
+public class Dice {
+    private int numberOfToss;
+    public Dice(int numberOfToss) {
+        this.numberOfToss = numberOfToss;
+    }
+    public int getNumberOfToss() {return this.numberOfToss;}
+    private int tossAndSum() {
+        Random random = new Random();
+        int result = 0;
+        for (int i=1; i<=getNumberOfToss(); i++) {
+            result = result + (random.nextInt(5) +1);
+        }
+        return result;
+    }
+
+
+
+//    int toss = dice.tossAndSum();
+//    Dice dice = new Dice(2); // for craps
+//    Dice dice = new Dice(5); // for yatzee
 
 }
