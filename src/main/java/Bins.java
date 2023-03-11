6c3ff2509ff1c991b6ff808eeca70629e0f8bb62
joe-trung <kieutrungtran@gmail.com>
2023-03-11 14:02:54 -0500

    Done Dice, Bin. Still have Simulation

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,24 @@
 
 public class Bins {
+    private int[] bin;
+    private int low, high;
+
+    public Bins(int low, int high) {
+        this.low = low;
+        this.high = high;
+        this.bin = new int[high-low];
+    }
+//    Bins results = new Bins(2, 12); // for bins from 2..12
+//    Integer numberOfTens = results.getBin(10); // returns the number of tens in the 10 bin
+
+    public int getBin(int iBin) {
+        return this.bin[iBin - low];
+    }
+
+    public void incrementBin(int increment) {
+        this.bin[increment - low] +=1;
+    }
+
+//    results.incrementBin(10); // should increment bin # 10
 
 }
